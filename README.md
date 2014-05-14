a59f82b199e19056a166cbc7ebddbc981dc880bc
staeiou <sgeiger@gmail.com>
2014-05-13 18:05:05 -0700

    Commit

--- README.md
+++
@@ -1,4 +0,0 @@
-robots.txt.php
-==============
-
-Dynamically generates a robots.txt file, allowing the user agent who requested it to access all
---
+++ robots.php
@@ -0,0 +1,9 @@
+<?php
+header('Content-Type:text/plain');
+
+echo "User-agent: " . $_SERVER['HTTP_USER_AGENT'] . "\r\n";
+
+echo "Allow: /" . "\r\n";
+
+echo "Sitemap: http://www.stuartgeiger.com/sitemap.xml.gz" . "\r\n";
+?>
