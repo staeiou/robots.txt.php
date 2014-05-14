d24a429cba5861a2fca3bd9bc07b20325db19726
staeiou <sgeiger@gmail.com>
2014-05-13 18:22:33 -0700

    Disallow all except requesting user

--- robots.php
+++ robots.php
@@ -1,8 +1,10 @@
 <?php
 header('Content-Type:text/plain');
 
-echo "User-agent: " . $_SERVER['HTTP_USER_AGENT'] . "\r\n";
+echo "User-agent: *" . "\r\n";
+echo "Disallow: /" . "\r\n";
 
+echo "User-agent: " . $_SERVER['HTTP_USER_AGENT'] . "\r\n";
 echo "Allow: /" . "\r\n";
 
 echo "Sitemap: http://www.stuartgeiger.com/sitemap.xml.gz" . "\r\n";
